a7b9e0c89aa037a125db71ed04bda1ef5b6016a7
Danila-Pechenev <danil-pechenev@mail.ru>
2023-03-01 00:25:38 +0300

    Remove all printings

--- data_collection.py
+++ data_collection.py
@@ -42,9 +42,6 @@ def file_generator(base_dir: str, n_cores: int, core: int):
             file_path = os.path.join(root, file)
             count += 1
             if count % n_cores == core:
-                if file_path.startswith("/bin/"):
-                    print(file_path)
-
                 yield file_path
 
 
