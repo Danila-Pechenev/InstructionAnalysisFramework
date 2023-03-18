c64a4d0d836d6951340e02adc516537c53eca2e2
Danila-Pechenev <danil-pechenev@mail.ru>
2023-03-19 00:17:39 +0300

    Fix bug with non-working folder ignoring

--- src/file_generators.py
+++ src/file_generators.py
@@ -22,10 +22,17 @@ def non_recursive_file_generator(base_dir: str, n_cores: int, core: int):
 def recursive_file_generator(base_dir: str, n_cores: int, core: int, ignore_folders: list[str]):
     count = -1
     for root, _, files in os.walk(base_dir):
-        if root in ignore_folders:
+        if is_ignored(root, ignore_folders):
             continue
         for file in files:
             file_path = os.path.join(root, file)
             count += 1
             if count % n_cores == core:
                 yield file_path
+
+
+def is_ignored(folder: str, ignore_folders: list[str]) -> bool:
+    for ignored_folder in ignore_folders:
+        if folder.startswith(ignored_folder):
+            return True
+    return False
