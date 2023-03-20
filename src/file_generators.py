b2f2de071082381d005991f0a387d0eeed82fecc
Danila-Pechenev <danil-pechenev@mail.ru>
2023-03-20 23:14:43 +0300

    Add tmp debug printing

--- src/file_generators.py
+++ src/file_generators.py
@@ -22,6 +22,8 @@ def non_recursive_file_generator(base_dir: str, n_cores: int, core: int):
 def recursive_file_generator(base_dir: str, n_cores: int, core: int, ignore_folders: list[str]):
     count = -1
     for root, _, files in os.walk(base_dir):
+        if "DataCollection" in root:
+            print(root)
         if is_ignored(root, ignore_folders):
             continue
         for file in files:
