dcff1f9b728b8f0fd77a81ddcc56cf5746a78990
Danila-Pechenev <danil-pechenev@mail.ru>
2023-03-20 23:18:28 +0300

    Change debug printing

--- src/file_generators.py
+++ src/file_generators.py
@@ -22,11 +22,12 @@ def non_recursive_file_generator(base_dir: str, n_cores: int, core: int):
 def recursive_file_generator(base_dir: str, n_cores: int, core: int, ignore_folders: list[str]):
     count = -1
     for root, _, files in os.walk(base_dir):
-        if "DataCollection" in root:
-            print(root)
         if is_ignored(root, ignore_folders):
+            print(f"Ignored: {root}")
             continue
         for file in files:
+            if "DataCollection" in root:
+                print(root)
             file_path = os.path.join(root, file)
             count += 1
             if count % n_cores == core:
