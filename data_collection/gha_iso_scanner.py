297756a8e4049e28dc27cd263a367bdf473b1ab9
Danila-Pechenev <danil-pechenev@mail.ru>
2023-03-24 12:02:05 +0300

    Add iso images data collection to `DC.yml`

---
+++ data_collection/gha_iso_scanner.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import subprocess as sp
+import click
+import json
+
+
+@click.command()
+@click.argument("json-file")
+@click.argument("key")
+@click.argument("table-path")
+def scan_iso(
+    json_file_path: str,
+    iso_key: str,
+    table_path: str,
+):
+    """Scans iso-image in GitHub Actions (runs-on: ubuntu).
+    Takes the path to the json-file, key of the image in it, and the path to the table to save."""
+    with open(json_file_path, "r") as read_file:
+        iso_info = json.load(read_file)["iso-images"]
+    url, objdump_package, objdump_command = None, None, None
+    for item in iso_info:
+        if item["key"] == iso_key:
+            url = item["url"]
+            objdump_package = item["objdump-package"]
+            objdump_command = item["objdump-command"]
+    if url is None:
+        raise KeyError("No such key.")
+    sp.run(["apt", "update"], capture_output=False)
+    sp.run(["apt", "install", "--yes", objdump_package], capture_output=False)
+    sp.run(["./url_iso_collection", url, table_path, objdump_command], capture_output=False)
+
+
+if __name__ == "__main__":
+    scan_iso()
