ac0779c9db10dd8b7df16568e3f38018f0ceb563
Danila-Pechenev <danil-pechenev@mail.ru>
2023-03-25 12:21:10 +0300

    Improve scripts for iso-files data collection

--- data_collection/local_iso_collection.sh
+++ data_collection/local_iso_collection.sh
@@ -7,11 +7,15 @@ set -euo pipefail
 # ./data_collection/local_iso_collection.sh <path_to_iso_file> <table_path> [optional: <objdump_command>
 # (default: objdump)]
 #
-trap 'fusermount -q -u "$root" || true; rm -r -f "$root"' EXIT
+function clean {
+    if [ -v root ]; then
+        fusermount -qu -- "$root" || true
+        rmdir -- "$root"
+    fi
+}
+trap clean EXIT
 
 objdump_command="${3:-objdump}"
 root=$(mktemp -d)
-fuseiso "$1" "$root"
-python data_collection/data_collection.py scan-folder -o "$objdump_command" -d "$root" -r "$2"
-fusermount -u "$root"
-rmdir "$root"
+fuseiso -- "$1" "$root"
+python data_collection/data_collection.py scan-folder -o "$objdump_command" -d "$root" -r -- "$2"
--- data_collection/url_iso_collection.sh
+++ data_collection/url_iso_collection.sh
@@ -7,10 +7,14 @@ set -euo pipefail
 # ./data_collection/url_iso_collection.sh <link_to_iso_file> <table_path> [optional: <objdump_command>
 # (default: objdump)]
 #
-trap 'rm -f "$iso_file"' EXIT
+function clean {
+    if [ -v iso_file ]; then
+        rm -f "$iso_file"
+    fi
+}
+trap clean EXIT
 
 objdump_command="${3:-objdump}"
 iso_file=$(mktemp imageXXX.iso)
-wget -O "$iso_file" "$1"
+wget -O -- "$iso_file" "$1"
 ./data_collection/local_iso_collection.sh "$iso_file" "$2" "$objdump_command"
-rm "$iso_file"
